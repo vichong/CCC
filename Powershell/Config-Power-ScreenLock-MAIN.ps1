63203c903c5e8cde2f5455f00b3c84cb5a605b4e
Victor Hong <vichong@gmail.com>
2024-08-02 16:17:41 +1000

    2

--- Powershell/Config-Power-ScreenLock-MAIN.ps1
+++ Powershell/Config-Power-ScreenLock-MAIN.ps1
@@ -2,29 +2,13 @@
 # To be hosted on GitHub
 
 # Function definitions
-function Test-Administrator {
-    $currentUser = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
-    return $currentUser.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)
-}
-
-function Write-Log {
-    param (
-        [string]$Message,
-        [string]$Level = "INFO"
-    )
-    $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
-    $logMessage = "$timestamp [$Level] $Message"
-    Add-Content -Path $logPath -Value $logMessage
-    Write-Host $logMessage
-}
-
 function Set-PowerSetting {
     param (
         [string]$SettingName,
         $SettingValue,
         [string]$PowerCondition
     )
-    # (Rest of the function body remains the same)
+    # Function implementation
 }
 
 function Set-DeviceLockParameters {
@@ -32,47 +16,22 @@ function Set-DeviceLockParameters {
         [bool]$Enabled,
         [int]$MaxInactivityTime
     )
-    # (Rest of the function body remains the same)
+    # Function implementation
 }
 
 function Set-HibernationState {
     param (
         [bool]$Enable
     )
-    # (Rest of the function body remains the same)
+    # Function implementation
 }
 
-# Main execution
 function Apply-PowerSettings {
     param (
         [hashtable]$Params
     )
-    try {
-        Write-Log "Starting power settings configuration"
-
-        Set-HibernationState -Enable $Params.EnableHibernation
-
-        foreach ($setting in $Params.AC.Keys) {
-            Set-PowerSetting -SettingName $setting -SettingValue $Params.AC[$setting] -PowerCondition "ac"
-        }
-
-        foreach ($setting in $Params.DC.Keys) {
-            Set-PowerSetting -SettingName $setting -SettingValue $Params.DC[$setting] -PowerCondition "dc"
-        }
-
-        # (Rest of the settings application remains the same)
-
-        Set-DeviceLockParameters -Enabled $Params.DevicePasswordEnabled -MaxInactivityTime $Params.MaxInactivityTimeDeviceLock
-
-        Write-Log "Power and sleep settings have been applied successfully"
-    }
-    catch {
-        Write-Log "An error occurred while applying power settings: $_" -Level "ERROR"
-    }
-    finally {
-        Write-Log "Power settings configuration completed"
-    }
+    # Function implementation that calls the other functions as needed
 }
 
-# Export the main function
+# Export only the main function that will be called from the tenant-specific script
 Export-ModuleMember -Function Apply-PowerSettings
